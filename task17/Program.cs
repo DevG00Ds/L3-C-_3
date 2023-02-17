3d6c6480bbd24c348b92c25a8e4dbf87ed369bb9
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-18 00:34:14 +0300

    added task17

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
 
 
@@ -8,23 +9,23 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(" Введите координату точки по Y");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if ( x > 0 && y > 0 )
+if (x > 0 && y > 0)
 {
     Console.WriteLine("Точка находится в 1 кардинате");
 }
-else if ( x < 0 && y > 0)
+else if (x < 0 && y > 0)
 {
     Console.WriteLine(" Точка находится во 2 координате");
 }
-else if ( x > 0 && y > 0 )
+else if (x > 0 && y > 0)
 {
     Console.WriteLine(" точка находится в 3 координате");
 }
-else if ( x > 0 && y < 0)
+else if (x > 0 && y < 0)
 {
     Console.WriteLine("  точка находится в 4 координате");
 }
-else 
+else
 {
     Console.WriteLine(" Невозможно определить ");
 }
