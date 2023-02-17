cfe51a2a08267a2b8c0e3eb84d433c1acc5efa1a
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-17 20:43:16 +0300

    added task17

---
+++ task17/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+Console.WriteLine(" Введите координату точки по Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Введите координату точки по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ( x > 0 && y > 0 )
+{
+    Console.WriteLine("Точка находится в 1 кардинате");
+}
+else if ( x < 0 && y > 0)
+{
+    Console.WriteLine(" Точка находится во 2 координате");
+}
+else if ( x > 0 && y > 0 )
+{
+    Console.WriteLine(" точка находится в 3 координате");
+}
+else if ( x > 0 && y < 0)
+{
+    Console.WriteLine("  точка находится в 4 координате");
+}
+else 
+{
+    Console.WriteLine(" Невозможно определить ");
+}
