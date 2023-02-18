d4ee2f6b13a6fa2f5a4e28d659a4201e2d64ece4
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-18 11:50:26 +0300

    finish task20

---
+++ task20/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,10
+// A (7,-5); B (1,-1) -> 7,21
+
+
+Console.WriteLine(" Введите координату по оси Х первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" Введите координату по оси У первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" Введите координату по оси Х Второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" Введите координату по оси У второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+Console.WriteLine(c);
